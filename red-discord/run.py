e06fbfad53a545fcac026ae3ef5258e9217f42ed
Pejter <piotrluczak1995@gmail.com>
2018-05-03 18:30:26 +0200

    Red-discord update
    Fixed cog installation on startup

--- red-discord/run.py
+++ red-discord/run.py
@@ -1,8 +1,9 @@
 import json
-from subprocess import call
-from glob import iglob
+import os
 import sys
+from subprocess import call
 
+repos_file = os.path.join('data', 'downloader', 'repos.json')
 interpreter = sys.executable
 if interpreter is None:
 	raise RuntimeError('Python interpreter not found')
@@ -12,7 +13,8 @@ def install(reqs):
 	if not reqs:
 		return
 
-	cmd = [interpreter, '-m', 'pip', 'install']
+	print(f'Installing {reqs}')
+	cmd = [interpreter, '-m', 'pip', 'install', '--upgrade', '--target', 'lib']
 	cmd.extend(reqs)
 	exit_code = call(cmd)
 	if exit_code != 0:
@@ -20,15 +22,27 @@ def install(reqs):
 
 
 def main():
-	for filename in iglob('/app/cogs/**/info.json', recursive=True):
-		with open(filename) as f:
-			data = json.load(f)
-		req_list = data.get('REQUIREMENTS', [])
-		install(req_list)
+	try:
+		with open(repos_file) as f:
+			repos = json.load(f)
+	except OSError:
+		repos = {}
+	for repo in repos.values():
+		for cog in repo.values():
+			if cog['INSTALLED']:
+				info_path = os.path.join(cog['folder'], 'info.json')
+				# if os.path.isfile(info_path):
+				try:
+					with open(info_file) as f:
+						cog_info = json.load(f)
+				except OSError:
+					cog_info = {}
+				reqs = cog_info.get('REQUIREMENTS')
+				install(reqs)
 
 
 if __name__ == '__main__':
-	print('Installing cog dependencies')
+	print('Installing cog dependencies...')
 	main()
 
 	red_cmd = [interpreter, './red.py', '--no-prompt']
